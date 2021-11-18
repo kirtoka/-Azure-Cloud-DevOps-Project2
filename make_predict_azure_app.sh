fc01a4e3fb2a70a6a8b66df38be449dd0a33d025
Natalia Chirtoca <chirtoca.natalia@gmail.com>
2021-11-18 06:44:25 +0000

    badge+appname

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>-app.azurewebsites.net:$PORT/predict 
+     -X POST https://flask-ml-project2-app.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
